44a75d70c3e44f7a53401ad3fd16057c3a4b2b38
zavad4 <lizavadovoska@gmail.com>
2020-05-19 20:13:05 +0300

    commit

--- main.js
+++ main.js
@@ -1,77 +1,64 @@
 'use strict';
-const { Telegraf } = require('telegraf');
+const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const request = require('request');
 const bot = new Telegraf('1241797755:AAGIIpoXru5SWv4mDO5PhSD7N4G3x91mEBQ');
 
 const token = 'aa36752d4a3159859afd0e84b3abf7cacab10018';
 
-
-async function getQualityByCoords(lon, lat) {
-    let res = 0;
+async function getQualityBy(type, ...args) {
+    let params = '';
+    if (type === 'city') params = args[0];
+    else if (type === 'coords') params = `geo:${args[1]};${args[0]}`;
     const options = {
         method: 'GET',
-        //url: `http://api.waqi.info/feed/${city}/?token=${token}`,
-        url: `https://api.waqi.info/feed/geo:${lat};${lon}/?token=${token}`
+        url: `https://api.waqi.info/feed/${params}/?token=${token}`,
     };
-    request(options, (error, response, body) => {
-        if (error) throw new Error(error);
-        res = JSON.parse(body).data.aqi;
-    });
-    console.log('it is res2 ' + res);
+    let res;
+    await new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                res = 'impossible to determine.';
+                reject(error);
+            }
+            resolve(JSON.parse(body));
+        });
+    })
+        .then(data => {
+            console.log(data.data.aqi);
+            res = data.data.aqi;
+        });
     return res;
 }
 
-const getQualityByCity = city => {
-    let res = 0;
-    const options = {
-        method: 'GET',
-        url: `http://api.waqi.info/feed/${city}/?token=${token}`,
-    };
-    request(options, (error, response, body) => {
-        if (error) throw new Error(error);
-        res = JSON.parse(body).data.aqi;
-    });
-    return res;
-};
-
-const arr = new Set();
+const cityRequested = new Set();
 
 bot.command('start', ctx => ctx.reply('Please location or city', Extra.markup(markup => markup.resize()
     .keyboard([
         markup.locationRequestButton('Send location'),
-        markup.button('Send city')
+        markup.button('Send city'),
     ])
     .oneTime()
 )));
 
-bot.on('location', ctx => {
+bot.on('location', async ctx => {
     const lon = ctx.message.location.longitude;
     const lat = ctx.message.location.latitude;
-    const index = getQualityByCoords(lon, lat);
-    console.log('it is index' + index);
+    const index = await getQualityBy('coords', lon, lat);
     ctx.reply(`Okey, I'll send you air quality index of this place every day. Now it is ${index}`);
 });
 
 bot.hears('Send city', ctx => {
-    arr.add(ctx.message.from.id);
+    cityRequested.add(ctx.message.from.id);
     ctx.reply('Okey, send me your city');
-    console.log(arr);
 });
 
-bot.on('text', ctx => {
-    if (arr.has(ctx.message.from.id)) {
-        const index = getQualityByCity(ctx.message.text);
+bot.on('text', async ctx => {
+    if (cityRequested.has(ctx.message.from.id)) {
+        const index = await getQualityBy('city', ctx.message.text);
         ctx.reply(`Okey, I'll send you air quality index of this place every day. Now it is ${index}`);
     }
-    arr.delete(ctx.message.from.id);
-    console.log(arr);
+    cityRequested.delete(ctx.message.from.id);
 });
 
 bot.launch();
-
-
-
-const a = getQualityByCoords(50.27, 30.30);
-//console.log('it is a ' + a);
-//getQualityByCity('Lviv');
