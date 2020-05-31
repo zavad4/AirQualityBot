4a0f790c6896839085ab065179af6f5778d19c03
zavad4 <lizavadovoska@gmail.com>
2020-05-31 14:15:36 +0300

    edits

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 'use strict';
-require('dotenv').config();
+require('dotenv').config({path : /home/liza/Desktop/univer./js./AirQualityBot/.env});
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const API_TOKEN = process.env.API_TOKEN;
 const API_URL = process.env.API_URL;
 const CORRECT_HOUR = 18;
 const CORRECT_MINUTES = 16;
+
+console.log(process.env);
 
 module.exports = {
   BOT_TOKEN,
