35451d4a958f714a3a8c657ec0395b15080cf404
zavad4 <lizavadovoska@gmail.com>
2020-06-01 16:08:18 +0300

    add commands

--- main.js
+++ main.js
@@ -1,105 +1,50 @@
 'use strict';
 const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
-const request = require('request');
-const CONSTANTS = require('./config.js');
-const { BOT_TOKEN, API_TOKEN, API_URL, CORRECT_HOUR, CORRECT_MINUTES } = CONSTANTS;
+
+const FUNCTIONS = require('./modules/functions.js');
+const { getQualityBy, setAnswer, replyFile, deleteUser, isInDatabase, mailing, } = FUNCTIONS;
+
+const CONSTANTS = require('./modules/config.js');
+const BOT_TOKEN = CONSTANTS.BOT_TOKEN;
 
 const bot = new Telegraf(BOT_TOKEN);
-const token = API_TOKEN;
 
 let databaseByCity = [];
 let databaseByCoords = [];
 const cityRequested = new Set();
-let currentHour = 0;
-let currentMinute = 0;
 
-async function getQualityBy(type, ...args) {
-  let params = '';
-  if (type === 'city') params = args[0];
-  else if (type === 'coords') params = `geo:${args[1]};${args[0]}`;
-  const options = {
-    method: 'GET',
-    url: API_URL + `${params}/?token=${token}`,
-  };
-  let res;
-  await new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body));
-      }
-    });
-  })
-    .then(data => {
-      res = data.data.aqi;
-    })
-    .catch(err => {
-      res = 'impossible to determine';
-      console.log(err);
-    });
-  return res;
-}
+bot.command('start', ctx => replyFile(ctx, './texts/gretting.txt'));
+bot.command('help', ctx => replyFile(ctx, './texts/manual.txt'));
+bot.command('scale', ctx => replyFile(ctx, './texts/scale.txt'));
 
-const sendQualityBy = type => async user => {
-  let index = 0;
-  if (type === 'city') index = await getQualityBy('city', user.city);
-  else if (type === 'coords') index = await getQualityBy('coords', user.lon, user.lat);
-  if (typeof index === 'number') {
-    bot.telegram.sendMessage(user.id, `Good morning! AQI of your place is ${index}`);
-  }
-};
-
-const setAnswer = index => {
-  let answer;
-  if (typeof index === 'number') {
-    answer = `Okey, I'll send you air quality index of this place every day. Now it is ${index}.`;
-  } else answer = 'It is impossible to determine. Please, enter city.';
-  return answer;
-};
-
-const deleteUser = (database, id) => database.filter(user => user.id !== id);
-
-let arr = [{ id: 1 }, { id: 2 }, { id: 1 }, { id: 3 }];
-deleteUser(arr, 1);
-
-const mailing = () => {
-  console.log(new Date().getUTCHours() + ':' + new Date().getUTCMinutes());
-  currentHour = new Date().getUTCHours();
-  currentMinute = new Date().getUTCMinutes();
-  if (currentHour === CORRECT_HOUR && currentMinute === CORRECT_MINUTES) {
-    databaseByCity.forEach(sendQualityBy('city'));
-    databaseByCoords.forEach(sendQualityBy('coords'));
-  }
-};
-
-bot.command('start', ctx => ctx.reply('Please location or city', Extra.markup(markup =>
-  markup.resize()
-    .keyboard([
-      markup.locationRequestButton('📍 Send location'),
-      markup.button('🏙 Send city'),
-    ])
-    .oneTime()
-)));
+bot.command('subscribe', ctx => ctx.reply('Please, send location or city.',
+  Extra.markup(markup =>
+    markup.resize()
+      .keyboard([
+        markup.locationRequestButton('📍 Send location'),
+        markup.button('🏙 Send city'),
+      ])
+      .oneTime()
+  )));
 
 bot.command('unsubscribe', ctx => {
   const id = ctx.message.chat.id;
-  console.log(id);
   databaseByCity = deleteUser(databaseByCity, id);
   databaseByCoords = deleteUser(databaseByCoords, id);
-  console.log(databaseByCity);
-  console.log(databaseByCoords);
   ctx.reply('By!');
 });
 
 bot.on('location', async ctx => {
   const lon = ctx.message.location.longitude;
   const lat = ctx.message.location.latitude;
-  databaseByCoords.push({ id: ctx.message.chat.id, lon, lat });
-  console.log(databaseByCoords);
-  const index = await getQualityBy('coords', lon, lat);
-  ctx.reply(setAnswer(index));
+  if (isInDatabase(databaseByCoords, ctx.message.chat.id)) {
+    ctx.reply('You are already subscribed, write /unsubscribe and tell the new place');
+  } else {
+    databaseByCoords.push({ id: ctx.message.chat.id, lon, lat });
+    const index = await getQualityBy('coords', lon, lat);
+    ctx.reply(setAnswer(index));
+  }
 });
 
 bot.hears('🏙 Send city', ctx => {
@@ -108,17 +53,21 @@ bot.hears('🏙 Send city', ctx => {
 });
 
 bot.on('text', async ctx => {
-  if (cityRequested.has(ctx.message.chat.id)) {
-    const index = await getQualityBy('city', ctx.message.text);
-    if (typeof index === 'number') {
-      databaseByCity.push({ id: ctx.message.chat.id, city: ctx.message.text });
+  const id = ctx.message.chat.id;
+  if (cityRequested.has(id)) {
+    if (isInDatabase(databaseByCity, id)) {
+      ctx.reply('You are already subscribed, write /unsubscribe and tell the new place');
+    } else {
+      const index = await getQualityBy('city', ctx.message.text);
+      if (typeof index === 'number') {
+        databaseByCity.push({ id, city: ctx.message.text });
+        cityRequested.delete(id);
+      }
+      ctx.reply(setAnswer(index));
     }
-    console.log(databaseByCity);
-    ctx.reply(setAnswer(index));
   }
-  cityRequested.delete(ctx.message.chat.id);
 });
 
-setInterval(mailing, 60000);
+setInterval(() => mailing(bot, databaseByCity, databaseByCoords), 60000);
 
 bot.launch();
---
+++ modules/functions.js
@@ -0,0 +1,86 @@
+'use strict';
+const request = require('request');
+const fs =  require('fs');
+
+const CONSTANTS = require('./config.js');
+const { API_TOKEN, API_URL, MAIL_TIME } = CONSTANTS;
+
+async function getQualityBy(type, ...args) {
+  let params = '';
+  if (type === 'city') params = args[0];
+  else if (type === 'coords') params = `geo:${args[1]};${args[0]}`;
+  const options = {
+    method: 'GET',
+    url: API_URL + `${params}/?token=${API_TOKEN}`,
+  };
+  let res;
+  await new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  })
+    .then(data => {
+      res = data.data.aqi;
+    })
+    .catch(err => {
+      res = 'impossible to determine';
+      console.log(err);
+    });
+  return res;
+}
+
+const sendQualityBy = (bot, type) => async user => {
+  let index = 0;
+  if (type === 'city') index = await getQualityBy('city', user.city);
+  else if (type === 'coords') {
+    index = await getQualityBy('coords', user.lon, user.lat);
+  }
+  if (typeof index === 'number') {
+    bot.telegram.sendMessage(user.id, `Good morning! AQI of your place is ${index}`);
+  }
+};
+
+const setAnswer = index => {
+  let answer;
+  if (typeof index === 'number') {
+    answer = `Okey, I'll send you air quality index of this place every day. Now it is ${index}.`;
+  } else answer = 'It is impossible to determine. Please, enter city.';
+  return answer;
+};
+
+const replyFile = (ctx, file) => ctx.reply(fs.readFileSync(file, 'utf8'));
+
+const deleteUser = (database, id) => database.filter(user => user.id !== id);
+
+const isInDatabase = (database, id) => {
+  let flag = false;
+  for (const iterator of database) {
+    if (id === iterator.id) {
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+const mailing = (bot, databaseByCity, databaseByCoords) => {
+  console.log(new Date().getUTCHours() + ':' + new Date().getUTCMinutes());
+  const currentHour = new Date().getUTCHours();
+  const currentMinute = new Date().getUTCMinutes();
+  if (currentHour === MAIL_TIME.hour && currentMinute === MAIL_TIME.minute) {
+    databaseByCity.forEach(sendQualityBy(bot, 'city'));
+    databaseByCoords.forEach(sendQualityBy(bot, 'coords'));
+  }
+};
+
+module.exports = {
+  getQualityBy,
+  setAnswer,
+  replyFile,
+  deleteUser,
+  isInDatabase,
+  mailing,
+};
